622a8951e8b843c9bb2de08d93fc8c6bdfa86621
Nguyễn Trần Chung <nguyentranchung52th@gmail.com>
2020-08-28 14:46:12 +0900

    update package name

--- src/Decomposer.php
+++ src/Decomposer.php
@@ -11,7 +11,7 @@ class Decomposer
      * in resolving its version number
      */
 
-    const PACKAGE_NAME = 'lubusin/laravel-decomposer';
+    const PACKAGE_NAME = 'laravel-vietnam/laravel-decomposer';
 
     /**
      * Initialise blank arrays for extra stats to be added
